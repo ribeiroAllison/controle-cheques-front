28fae9006defe7b2f620f48331b864941c863d37
ribeiroAllison <allisonribeiro@gmail.com>
2023-05-08 13:16:32 -0300

    design navbar

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import style from '../styles/Header.module.css'
+import Link from 'next/link'
+
+export default function Header (){
+
+    return(
+        <>
+            <header className={style.header}> 
+
+                <div className={style.imgCtr}>
+                    <img className={style.img}  src="https://multitoc.com.br/wp-content/uploads/2021/08/LOGO-MULTITOC-GRANDE-PRETA-1024x266.png" />
+                </div>
+                
+                
+                <nav className={style.navBar}>
+                    
+                    <div className={style.menu}>
+                        <h2>Cadastro</h2>
+                        <div className={style.option}>
+                            <h3><Link href="/clientes">Clientes</Link></h3>
+                            <h3><Link href="">Destinos</Link></h3>
+                            <h3><Link href="">Grupos</Link></h3>
+                        </div>
+                    </div>
+                    
+                </nav>
+            </header>
+        </>
+    )
+}
---
+++ src/pages/clientes.js
@@ -0,0 +1,10 @@
+
+
+export default function Clientes() {
+
+    return(
+        <>
+            <h1>Clientes</h1>
+        </>
+    )
+}
