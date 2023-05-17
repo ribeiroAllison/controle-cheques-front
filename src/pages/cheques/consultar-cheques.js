e74b62b796a6e9d3b37115988c4057c638d476e5
ribeiroAllison <allisonribeiro@gmail.com>
2023-05-17 20:36:50 -0300

    working edit input fields

--- src/pages/cheques/consultar-cheques.js
+++ src/pages/cheques/consultar-cheques.js
@@ -28,6 +28,25 @@ export default function ConsultarCheques() {
         setFormValues({ ...formValues, [name]: value });
     };
 
+    const [editFormValues, setEditFormValues] = useState(
+        {
+            cliente: "",
+            cliente_cod: null,
+            número_cheque: null,
+            valor: null,
+            data_venc: null,
+            compensado: null,
+            vencido: null,
+            destino_id: null
+
+            
+        }
+    );
+
+    const handleEditInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditFormValues({ ...editFormValues, [name]: value });
+    };
     
 
     const [clientList, setClientList] = useState('');
@@ -53,26 +72,39 @@ export default function ConsultarCheques() {
 
     const [searchResult, setSearchResult] = useState([{}]);
 
-    const findClient = () => {
+    const findClient = (formValues, id, targetField) => {
 
+        
         if(clientList){
             const foundClientByName = clientList.filter(client => client.nome.toLowerCase().includes(formValues.cliente.toLowerCase()));
             setSearchResult(foundClientByName);
-            searchResult.length === 0 || !document.getElementById('cliente').value ? document.getElementById('searchBox').style.display = 'none' : document.getElementById('searchBox').style.display = 'block'
+            searchResult.length === 0 || !document.getElementById(targetField).value ? document.getElementById(id).style.display = 'none' : document.getElementById(id).style.display = 'block'
             
         }
 
     }
 
     useEffect(() => {
-        findClient()
+        findClient(formValues, "searchBox", 'cliente')
     },[formValues.cliente])
+
+    useEffect(() => {
+        findClient(editFormValues, "searchBoxEdit", 'clienteEdit')
+    },[editFormValues.cliente])
 
 
     const handleClick = (e) =>{
         setFormValues({...formValues, cliente_cod: e.target.value})
         document.getElementById('searchBox').style.display = 'none';
         document.getElementById('cliente').value = e.target.innerHTML;
+
+
+    }
+
+    const handleEditClick = (e) =>{
+        setEditFormValues({...editFormValues, cliente_cod: e.target.value})
+        document.getElementById('searchBoxEdit').style.display = 'none';
+        document.getElementById('clienteEdit').value = e.target.innerHTML;
 
     }
 
@@ -94,6 +126,7 @@ export default function ConsultarCheques() {
     }   
 
     const [chequesList, setChequeslist] = useState();
+    const [frozenParams, setFrozenParams] = useState(); 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -117,11 +150,23 @@ export default function ConsultarCheques() {
         if(response.ok){
             let jsonResponse = await response.json();
             setChequeslist(jsonResponse);
-            clearInputs();
+            clearInputs(e, 'input');
                 
         }  else {
             console.error('Erro ao obter os cheques da API.');
         }
+
+        setFrozenParams({
+            cliente: "",
+            cliente_cod: formValues.cliente_cod,
+            data_init: formValues.data_init,
+            data_fim: formValues.data_fim,
+            compensado: formValues.compensado,
+            destino_id: formValues.destino_id,
+            vencido: formValues.vencido
+        }
+            
+        )
 
         setFormValues({
             cliente: "",
@@ -136,7 +181,7 @@ export default function ConsultarCheques() {
 
     const handleClear = (e) => {
         e.preventDefault();
-        clearInputs();
+        clearInputs(e, 'input');
         setFormValues({
             cliente: "",
             cliente_cod: null,
@@ -148,9 +193,76 @@ export default function ConsultarCheques() {
         })
     }
 
+    const handleClearEdit = (e) => {
+        e.preventDefault();
+        clearInputs(e, 'editInput');
+        setEditFormValues({
+            cliente: "",
+            cliente_cod: null,
+            número_cheque: null,
+            valor: null,
+            data_venc: null,
+            compensado: null,
+            vencido: null,
+            destino_id: null
+        })
+    }
+
     const transformDate = (data) =>{
         const date = new Date(data);
         return new Intl.DateTimeFormat('pt-BR').format(date);
+    }
+
+    const refreshSearch = async () =>{
+    
+            const searchParams = new URLSearchParams({
+                    cliente_cod: frozenParams.cliente_cod ? frozenParams.cliente_cod : '',
+                    data_init: frozenParams.data_init ? frozenParams.data_init : '',
+                    data_fim: frozenParams.data_fim ? frozenParams.data_fim : '',
+                    compensado: frozenParams.compensado ? frozenParams.compensado : '',
+                    destino_id: frozenParams.destino_id ? frozenParams.destino_id : '',
+                    vencido: frozenParams.vencido ? frozenParams.vencido : ''
+            })
+    
+            const response = await fetch(`${baseURL}/cheques?${searchParams.toString()}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            
+            if(response.ok){
+                let jsonResponse = await response.json();
+                setChequeslist(jsonResponse);
+                    
+            }  else {
+                console.error('Erro ao obter os cheques da API.');
+            }
+    
+    }
+
+    const handleDelete = async (id) =>{
+        
+        try{
+            const response = await fetch(`${baseURL}/cheques`,{
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: id
+                })
+            }
+            );
+
+            if(response.ok){
+                alert('Cheque removido com sucesso!')
+            }
+        } catch(error){
+            alert('Erro:' + error.message)
+        }
+
+        refreshSearch()
     }
 
     return(
@@ -232,10 +344,81 @@ export default function ConsultarCheques() {
             </fieldset>
 
             <ChequesHeader />
+            <form className={style.formCtr} id={style.clienteForm}>
+
+                <div className={`${style.inputCtr} ${style.nameCtr}`} id="clienteBox" >
+                    <h4>Cliente:</h4>
+                    <input type="text" name="cliente" onChange={handleEditInputChange} id="clienteEdit" placeholder="Pesquise o Cliente" className="editInput"/>
+                    <div className={style.searchBox} id="searchBoxEdit">
+                        <select size={4} id={`${style.clienteSelect} editInput`} onChange={handleEditInputChange}>
+                            {
+                                searchResult.map(client => <option onClick={handleEditClick} key={client.cod} value={client.cod}>{client.nome}</option>)
+                            }
+                        </select>
+                    </div>
+                    
+                </div>
+                <div className={style.inputCtr}>
+                    <h4>No. Cheque</h4>
+                    <input type="text" onChange={handleEditInputChange} name="número_cheque" className="editInput"/>
+                </div>
+                <div className={style.inputCtr}>
+                    <h4>Valor</h4>
+                    <input type="number" onChange={handleEditInputChange} name="valor" className="editInput"/>
+                </div>
+                <div className={style.inputCtr}>
+                    <h4>Vencimento</h4>
+                    <input type="date" onChange={handleEditInputChange} name="data_venc" className="editInput"/>
+                </div>
+                    
+                    <fieldset className={style.formCtr}>
+                        <legend>Status</legend>
+                        <div className={style.inputCtr} >
+                            <h4>Compensado:</h4>
+                            <select className={`${style.select} editInput`} name="compensado" id="compensado" onChange={handleEditInputChange} >
+                                <option value={null}></option>
+                                <option value={false}>Não</option>
+                                <option value={true}>Sim</option>
+                                
+                            </select>
+                        </div>
+
+                        <div className={style.inputCtr} >
+                            <h4>Vencido:</h4>
+                            <select className={`${style.select} editInput`} name="vencido" id="vencido" onChange={handleEditInputChange}>
+                                <option value={null}></option>
+                                <option value={false}>Não</option>
+                                <option value={true}>Sim</option>
+                                
+                            </select>
+                        </div>
+
+                </fieldset>
+
+                <div className={style.inputCtr} >
+                    <h4>Destino</h4>
+                    <select name="destino_id" onChange={handleEditInputChange} placeholder="Selecione Vendedor" className={`${style.select} editInput`}>
+                        <option key="0"></option>
+                        {
+                            destinoList && destinoList.map(destino => <option key={destino.id} value={destino.id}>{destino.nome}</option>)
+                        }
+                    </select>
+                </div>
+
+                <div className={style.buttonCtr}>
+                        <button type="submit" className={style.button} id="editaCheque">Buscar</button>
+                        <button className={style.button} onClick={handleClearEdit}>Limpar</button>
+                </div>
+                
+            </form>
+
+
+
+            
             <table className="table">
                 <thead>
                     <tr>
-                        <th>Código</th>
+                        <th>Cod. Cliente</th>
                         <th>Cliente</th>
                         <th>Grupo</th>
                         <th>No. Cheque</th>
@@ -263,7 +446,7 @@ export default function ConsultarCheques() {
                         <td id={`linha${cheque.id}`} >{cheque.linha}</td>
                         <td id={`destino${cheque.id}`} >{cheque.destino}</td>
                         <td> <img src="/images/edit.svg" name={cheque.id}/></td>
-                        <td> <img src="/images/trash-bin.svg" /></td>
+                        <td> <img src="/images/trash-bin.svg" onClick={() => handleDelete(cheque.id)}/></td>
                     
                     </tr>
                 ))}
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,8 @@
-export const clearInputs = (e) =>{
+export const clearInputs = (e, inputId) =>{
         
     e && e.preventDefault();
     
-    const inputs = document.getElementsByClassName('input');
+    const inputs = document.getElementsByClassName(inputId);
     for(let input of inputs){
         input.value = ""
     }
