f55cce8b845667174c483a47ea4321b7efc60a39
ALLISON RIBEIRO <allisonribeiro@MacBook-Air-de-ALLISON.local>
2023-06-26 12:55:11 -0300

    remove unused import in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@/components/Header';
 import ChequeControl from '@/components/ChequeControl';
-import ChequeControlEstornados from '@/components/ChequeControlEstornados';
+
 
 
 export default function Home() {
