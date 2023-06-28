fb6ae310def55e58e2988d8e8ff430288d601cf7
ALLISON RIBEIRO <allisonribeiro@MacBook-Air-de-ALLISON.local>
2023-06-28 11:28:01 -0300

    fix middleware url bug

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server';
+import { baseURL } from './utils/url';
 
 export function middleware(request) {
     const token = request.cookies.get('token')?.value // pega o token na request
 
     if (!token) {
-        return NextResponse.redirect('localhost:3000/', {
+        return NextResponse.redirect(baseURL, {
             'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20`,
         })
     }
