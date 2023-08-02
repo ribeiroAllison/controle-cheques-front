6c32aeed2c163a0e5d4f53b54e80af7051c5353f
erickunzel <kunzel.eric@gmail.com>
2023-08-02 18:01:32 -0300

    feat: installed eslint + started landpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,48 @@
-import Button from '@/components/Button';
-import HeaderLogin from '@/components/HeaderLogin';
-import Link from 'next/link';
-import styles from '../styles/IndexLayout.module.css'
+import Button from "@/components/Button";
+import Link from "next/link";
+import header from "../styles/landpage/Header.module.css";
+import mainSection from "../styles/landpage/MainSection.module.css";
 
 export default function Home() {
   return (
     <>
-      <HeaderLogin />
-      <div className={styles.mainWrapper}>
-        <h1 className={styles.mainTitle}>Seja bem-vindo!</h1>
-        <h2 className={styles.mainSubTitle}>Caso já possua cadastro, clique no botão de Login.</h2>
-        <div className={styles.btns}>
-        <Link href="/home/login"><Button>Login</Button></Link>
-        <Link href="/home/cadastro"><Button>Cadastre-se</Button></Link>
+      {/* HEADER */}
+      <header className={header.headerWrapper}>
+        <div className={header.imgContainer}>
+          <img src="/images/cheques-logo.svg" alt="" />
         </div>
-      </div>
+        <nav>
+          <ul className={header.menuWrapper}>
+            <li>Funcionalidades</li>
+            <li>Planos</li>
+            <li>Fale conosco</li>
+          </ul>
+        </nav>
+        <div className={header.btnContainer}>
+          <Button>Teste grátis</Button>
+          <Button>Login</Button>
+        </div>
+      </header>
+
+      {/* MAIN BANNER SECTION */}
+      <main className={mainSection.container}>
+        <div className={mainSection.textContainer}>
+          <h1>
+            Esqueça a burocracia e <strong>foque nos novos horizontes</strong>{" "}
+            para o seu negócio.
+          </h1>
+          <h2>Sistema online de gestão de cheques</h2>
+          <Button>Teste grátis</Button>
+        </div>
+      </main>
+
+      {/* ADVANTAGES SECTION */}
+      <section>
+        <h1>Teste grátis</h1>
+      </section>
+      {/* CARDS SECTION */}
+      {/* FORM SECTION */}
+      {/* FOOTER SECTION */}
     </>
-  )
+  );
 }
