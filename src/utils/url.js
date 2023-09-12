67d88d3383cba3ce2be434f1a59e72e3919165bd
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2023-09-12 13:55:51 -0300

    change db to dev

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,8 +1,8 @@
 //dev-DB:
-// const baseURL = "https://controle-cheques-dev-b22e1941ba59.herokuapp.com";
+const baseURL = "https://controle-cheques-dev-b22e1941ba59.herokuapp.com";
 
 // production-DB:
-const baseURL = 'https://controle-cheques-2cfc3b4dc745.herokuapp.com';
+// const baseURL = 'https://controle-cheques-2cfc3b4dc745.herokuapp.com';
 
 //local-Db
 // const baseURL = 'http://localhost:3351'
