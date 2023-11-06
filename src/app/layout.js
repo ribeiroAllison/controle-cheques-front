8676fd672f2546807fb7059245a88fa73ab3070d
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2023-11-06 12:16:20 -0300

    start setting payment form

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,12 @@ import "./global.css";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function RootLayout({ children }) {
+
   return (
     <html>
       <head>
         <link rel="icon" type="image/x-icon" href="/images/favicon.svg" />
+        <script src="https://assets.pagseguro.com.br/checkout-sdk-js/rc/dist/browser/pagseguro.min.js"></script>
       </head>
       <body>{children}</body>
     </html>
