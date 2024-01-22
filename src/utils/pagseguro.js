18abaaf9bb300a57d96f4e492a19a33f0592ee7f
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2024-01-22 16:51:37 -0300

    change db back to dev

--- src/utils/pagseguro.js
+++ src/utils/pagseguro.js
@@ -1,10 +1,10 @@
 //DEVELOPMENT
-// const baseURL = 'https://sandbox.api.assinaturas.pagseguro.com';
-// const token = '753238BB80874253A26413CAE2C2B9B2';
+const baseURL = 'https://sandbox.api.assinaturas.pagseguro.com';
+const token = '753238BB80874253A26413CAE2C2B9B2';
 
 //PRODUCION
-const baseURL = 'https://api.assinaturas.pagseguro.com';
-const token = '41d14f3e-8d7a-44af-9f4d-d00a8c53f71cf4012407439d8c68df40e1b18c70be43762e-400d-4570-87ce-13fe905b2ad0';
+// const baseURL = 'https://api.assinaturas.pagseguro.com';
+// const token = '41d14f3e-8d7a-44af-9f4d-d00a8c53f71cf4012407439d8c68df40e1b18c70be43762e-400d-4570-87ce-13fe905b2ad0';
 
 export {baseURL, token}
 
