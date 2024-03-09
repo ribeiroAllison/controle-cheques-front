d26cf1ec0e62f7814085c475ed425cc756039df8
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2024-03-09 09:30:03 -0300

    set up google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,19 +7,19 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" type="image/x-icon" href="/images/favicon.svg" />
         <script src="https://assets.pagseguro.com.br/checkout-sdk-js/rc/dist/browser/pagseguro.min.js"></script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ER8WTG9D1Y"></script>
-<script>
-  dangerouslySetInnerHTML={{
-    __html: `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ER8WTG9D1Y"></script>
+        <script>
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-    gtag('config', 'G-ER8WTG9D1Y');
-    `
-  }}
-  
-</script>
+            gtag('config', 'G-ER8WTG9D1Y');
+            `
+          }}
+          
+        </script>
         
       </head>
       <body>{children}</body>
