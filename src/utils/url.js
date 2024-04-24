796bf0460692da768552089ff230b9726ba9b3a7
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2024-04-24 20:37:51 -0300

    change db to main

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,14 +1,14 @@
 //dev-DB:
-const baseURL = "https://controle-cheques-dev-b22e1941ba59.herokuapp.com";
-const planoMensal = 'PLAN_8E129C7C-BB73-48F2-B265-DDDFDBAECF19';
-const planoTrimestral = 'PLAN_E4CE4617-A23C-4A30-AD90-B5C4D6D09BD7';
-const planoAnual = 'PLAN_530536A7-941B-45D0-A350-9E5D219A42C0';
+// const baseURL = "https://controle-cheques-dev-b22e1941ba59.herokuapp.com";
+// const planoMensal = 'PLAN_8E129C7C-BB73-48F2-B265-DDDFDBAECF19';
+// const planoTrimestral = 'PLAN_E4CE4617-A23C-4A30-AD90-B5C4D6D09BD7';
+// const planoAnual = 'PLAN_530536A7-941B-45D0-A350-9E5D219A42C0';
 
 // production-DB:
-// const baseURL = 'https://controle-cheques-2cfc3b4dc745.herokuapp.com';
-// const planoMensal = 'PLAN_77225D8F-BF7F-4531-A563-77051192B2D4';
-// const planoTrimestral = 'PLAN_21EE7E43-C3F5-4644-B55B-CD2FFC37A545';
-// const planoAnual = 'PLAN_0B04A8E2-41EE-43F8-A726-382E954E7B42';
+const baseURL = 'https://controle-cheques-2cfc3b4dc745.herokuapp.com';
+const planoMensal = 'PLAN_77225D8F-BF7F-4531-A563-77051192B2D4';
+const planoTrimestral = 'PLAN_21EE7E43-C3F5-4644-B55B-CD2FFC37A545';
+const planoAnual = 'PLAN_0B04A8E2-41EE-43F8-A726-382E954E7B42';
 
 
 
